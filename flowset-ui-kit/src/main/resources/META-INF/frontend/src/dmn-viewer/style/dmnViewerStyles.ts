1222967504897aa395e20706342c137170d61a53
Maria Orlova <m.orlova@haulmont.com>
2026-02-12 10:28:08 +0400

    Support for displaying decision definition only without DRD flowset/flowset-control-community#137

---
+++ flowset-ui-kit/src/main/resources/META-INF/frontend/src/dmn-viewer/style/dmnViewerStyles.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Haulmont 2026. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
+import {css} from 'lit';
+
+export const dmnViewerStyles = css`
+   .no-drd-button .view-drd {
+       display: none;
+   }
+`;
